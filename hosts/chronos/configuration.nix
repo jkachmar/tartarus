606fe45807edf7ffde1aaab1d50b664d62b2b027
jkachmar <git@jkachmar.com>
2025-10-05 20:48:27 -0400

    misc(chronos): add plex back

--- hosts/chronos/configuration.nix
+++ hosts/chronos/configuration.nix
@@ -42,6 +42,29 @@
     }
   ];
 
+  # TODO: Move this out to a separate media module or something.
+  users.groups.media.gid = 1010;
+
+  services.plex.enable = true;
+  users.users.plex.extraGroups = [ "media" ];
+  # FIXME: reverse proxy these behind nginx & close the ports off.
+  networking.firewall = {
+    allowedTCPPorts = [
+      32400
+      3005
+      8324
+      32469
+    ];
+    allowedUDPPorts = [
+      1900
+      5353
+      32410
+      32412
+      32413
+      32414
+    ];
+  };
+
   # Hardware survey results.
   boot = {
     initrd = {
